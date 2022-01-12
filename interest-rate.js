0a29ffedb5f2cbacc1a6fef0ac5ad1cb7be53c6e
Nicholas <n.paz4444@gmail.com>
2022-01-11 23:48:50 -0500

    working with const variables

---
+++ interest-rate.js
@@ -0,0 +1,8 @@
+// instead of using lets, use const. This is similar to final variables in java
+// let interestRate = 0.3;
+// interestRate = 1;
+
+const interestRate = 1;
+console.log(interestRate);
+
+// constant
