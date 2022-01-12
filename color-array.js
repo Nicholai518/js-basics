c72563fcfb0cb5956f04a197d08cb9b5cc41941a
Nicholas <n.paz4444@gmail.com>
2022-01-12 18:03:46 -0500

    array exercise

---
+++ color-array.js
@@ -0,0 +1,31 @@
+// Initialized with empty array
+// let selectedColors = [];
+
+// Initialized with colors
+let selectedColors = ['red', 'blue'];
+
+// displays all elements of the array
+console.log(selectedColors);
+
+// Displays element at index 0
+console.log(selectedColors[0]);
+
+// add another element to the array
+selectedColors[2] = 'green';
+
+// Dislay all elements 
+console.log(selectedColors);
+
+// Update index 2, to be a Number
+selectedColors[2] = 7;
+
+// Dislay all elements 
+console.log(selectedColors);
+
+// In console typeOf selectedColors is an "object"
+// typeOf selectedColors[0] is an "string"
+// typeOf selectedColors[1] is an "string"
+// typeOf selectedColors[2] is an "number"
+
+// Returns the number of elements in an array
+console.log(selectedColors.length);
