d05d9be8694c197aa8ba3057ec29285bdd288acb
Nicholas <n.paz4444@gmail.com>
2022-01-12 16:12:31 -0500

    Object example

---
+++ reference-types.js
@@ -0,0 +1,27 @@
+// Objects
+// Array
+// Function
+
+
+// Object
+// Key Value pairs
+let person = {
+    // K      V
+    name: 'Nicholai',
+    age: 30
+};
+
+
+// how to change properties
+
+// Dot notation  (More common, cleaner and easier)
+person.name = 'John';
+
+// Bracket Notation
+person['name'] = 'Tommy';
+
+// Can also use this for Bracket Notation
+let selection = 'name';
+person[selection] = "Dante"
+
+console.log(person.name);
