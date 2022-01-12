60c1285ed064fed7f40f149302d4357863ad88f7
Nicholas <n.paz4444@gmail.com>
2022-01-11 19:19:23 -0500

    Created index.js file, referenced this file within our script tag within index.html

---
+++ index.js
@@ -0,0 +1,3 @@
+        // This is how to do a comment in JavaScript
+        // Go to webpage, right click, press "Inspect" and in the console you will see "Hello World"
+        console.log('Hello World');
