71d5abc374ef4a8f8c0894edb874047680297687
Nicholas <n.paz4444@gmail.com>
2022-01-12 15:21:42 -0500

    added console commands comments to alter dynamic type variables

---
+++ primitive-types.js
@@ -0,0 +1,21 @@
+// String
+// Number
+// Boolean
+// undefined
+// null
+
+
+let friendsName = 'Kevin';      // String Literal
+let age = 25;                   // Number Literal
+let isApproved = true;          // Boolean Literal
+let firstName;                // undefined
+let middleName = undefined;     // undefined
+let lastName = null;            // null
+
+console.log(friendsName);
+
+// These are considered dynamic types
+// by opening the console, you can type typeOf friendsName and it will show "String"
+// you can then type friendsName = 2;
+// then, typeOf friendsName will show Number
+// Dynamic = are able to change
