10fc32c443c42df0907cd5983c7009cfcfc9a82f
Nicholas <n.paz4444@gmail.com>
2022-01-11 20:49:59 -0500

    variables exercise

---
+++ variables.js
@@ -0,0 +1,18 @@
+        // variables
+        // Rule: Variables cannot be a reserved keyword
+        // Rule: Names should be meaningful
+        // Rule: Cannot start with a number
+        // Rule: Cannot contain a space or hyphen -
+        // Rule: Case-sensitive
+        // Rule: Can separate with a comma
+
+        // single qoutes or double qoutes can be used. Single is more common in JavaScript
+        let name = 'Nicholai';
+        console.log(name);
+
+        // same line
+        //let firstName = 'Bob' , lastName = 'Saget';
+
+        // different line
+        let firstName = 'Bob';
+        let lastName = 'Saget';
